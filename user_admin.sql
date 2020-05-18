88c618dab54316561267e3a818582895108e1299
Bilal <bilaldekar@gmail.com>
2020-05-18 19:35:08 +0200

    Update user_admin.sql

--- user_admin.sql
+++ user_admin.sql
@@ -4,7 +4,7 @@
 
 -- Dumped from database version 9.3.3
 -- Dumped by pg_dump version 9.3.3
--- Started on 2020-02-26 15:52:09
+-- Started on 2020-05-18 18:34:23
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -14,7 +14,7 @@ SET check_function_bodies = false;
 SET client_min_messages = warning;
 
 --
--- TOC entry 172 (class 3079 OID 11750)
+-- TOC entry 175 (class 3079 OID 11750)
 -- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
 --
 
@@ -22,8 +22,8 @@ CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 
 
 --
--- TOC entry 1942 (class 0 OID 0)
--- Dependencies: 172
+-- TOC entry 1961 (class 0 OID 0)
+-- Dependencies: 175
 -- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
 --
 
@@ -32,8 +32,40 @@ COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
 
 SET search_path = public, pg_catalog;
 
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
 --
--- TOC entry 171 (class 1259 OID 49547)
+-- TOC entry 172 (class 1259 OID 24580)
+-- Name: role; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
+--
+
+CREATE TABLE role (
+    id bigint NOT NULL,
+    description character varying
+);
+
+
+ALTER TABLE public.role OWNER TO postgres;
+
+--
+-- TOC entry 173 (class 1259 OID 24588)
+-- Name: role_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE role_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.role_id_seq OWNER TO postgres;
+
+--
+-- TOC entry 170 (class 1259 OID 16583)
 -- Name: user_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -47,21 +79,30 @@ CREATE SEQUENCE user_id_seq
 
 ALTER TABLE public.user_id_seq OWNER TO postgres;
 
-SET default_tablespace = '';
+--
+-- TOC entry 174 (class 1259 OID 33326)
+-- Name: user_role; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
+--
 
-SET default_with_oids = false;
+CREATE TABLE user_role (
+    id_user bigint NOT NULL,
+    id_role bigint NOT NULL
+);
+
+
+ALTER TABLE public.user_role OWNER TO postgres;
 
 --
--- TOC entry 170 (class 1259 OID 49539)
+-- TOC entry 171 (class 1259 OID 16585)
 -- Name: users; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
 --
 
 CREATE TABLE users (
     id bigint NOT NULL,
-    first_name character varying,
-    last_name character varying,
-    user_name character varying,
-    email character varying,
+    first_name character varying(30) NOT NULL,
+    last_name character varying(30) NOT NULL,
+    user_name character varying(30) NOT NULL,
+    email character varying(50),
     active boolean
 );
 
@@ -69,27 +110,78 @@ CREATE TABLE users (
 ALTER TABLE public.users OWNER TO postgres;
 
 --
--- TOC entry 1943 (class 0 OID 0)
--- Dependencies: 171
--- Name: user_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+-- TOC entry 1951 (class 0 OID 24580)
+-- Dependencies: 172
+-- Data for Name: role; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('user_id_seq', 1, false);
-
-
---
--- TOC entry 1933 (class 0 OID 49539)
--- Dependencies: 170
--- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-COPY users (id, first_name, last_name, user_name, email, active) FROM stdin;
-1	Bilal	Dekar	bilal.dekar	bilaldekar@gmail.com	t
+COPY role (id, description) FROM stdin;
+1	ADMIN
+2	USER
 \.
 
 
 --
--- TOC entry 1825 (class 2606 OID 49546)
+-- TOC entry 1962 (class 0 OID 0)
+-- Dependencies: 173
+-- Name: role_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('role_id_seq', 2, true);
+
+
+--
+-- TOC entry 1963 (class 0 OID 0)
+-- Dependencies: 170
+-- Name: user_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('user_id_seq', 24, true);
+
+
+--
+-- TOC entry 1953 (class 0 OID 33326)
+-- Dependencies: 174
+-- Data for Name: user_role; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY user_role (id_user, id_role) FROM stdin;
+1	1
+1	2
+\.
+
+
+--
+-- TOC entry 1950 (class 0 OID 16585)
+-- Dependencies: 171
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY users (id, first_name, last_name, user_name, email, active) FROM stdin;
+1	bilal	dekar	bilal.dekar	bilaldekar@gmail.com	t
+\.
+
+
+--
+-- TOC entry 1837 (class 2606 OID 24587)
+-- Name: role_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
+--
+
+ALTER TABLE ONLY role
+    ADD CONSTRAINT role_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 1839 (class 2606 OID 33330)
+-- Name: user_role_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
+--
+
+ALTER TABLE ONLY user_role
+    ADD CONSTRAINT user_role_pkey PRIMARY KEY (id_user, id_role);
+
+
+--
+-- TOC entry 1835 (class 2606 OID 16592)
 -- Name: users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
 --
 
@@ -98,8 +190,26 @@ ALTER TABLE ONLY users
 
 
 --
--- TOC entry 1941 (class 0 OID 0)
--- Dependencies: 5
+-- TOC entry 1841 (class 2606 OID 33336)
+-- Name: fk_id_role; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY user_role
+    ADD CONSTRAINT fk_id_role FOREIGN KEY (id_role) REFERENCES role(id);
+
+
+--
+-- TOC entry 1840 (class 2606 OID 33331)
+-- Name: fk_id_user; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY user_role
+    ADD CONSTRAINT fk_id_user FOREIGN KEY (id_user) REFERENCES users(id);
+
+
+--
+-- TOC entry 1960 (class 0 OID 0)
+-- Dependencies: 6
 -- Name: public; Type: ACL; Schema: -; Owner: postgres
 --
 
@@ -109,7 +219,7 @@ GRANT ALL ON SCHEMA public TO postgres;
 GRANT ALL ON SCHEMA public TO PUBLIC;
 
 
--- Completed on 2020-02-26 15:52:09
+-- Completed on 2020-05-18 18:34:23
 
 --
 -- PostgreSQL database dump complete
